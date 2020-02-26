05f4a74e5100227e548f98f29eb16937fb423731
BeataMaro <beatamaro@interia.pl>
2020-02-26 12:25:23 +0100

    Project transfer

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,4 @@
-//Hero title animation
-
 const hdr = document.getElementById('header');
-// let lastTitle = document.getElementById('last');
 const hamburger = document.querySelector('[data-hamburger-menu]');
 const navItems = document.getElementById('navItems');
 const scrollArrow = document.querySelector('.scrollArrow');
@@ -9,24 +6,18 @@ const iconHero = document.querySelector('.scrollArrow__icon--hero');
 const arrowIcon = document.getElementById('heroIcon');
 const submitBtn = document.getElementById('submitBtn');
 const goToTopBtn = document.getElementById('goToTop');
-
 const heroTitle = document.querySelector('.hero__title');
 const heroText = heroTitle.textContent;
 const heroLetters = heroText.split('');
 heroTitle.textContent = '';
 
-
-let loadAnimation = setTimeout(function () {
+//Hero title animation
+document.addEventListener('DOMContentLoaded', () => {
 
   for (let i = 0; i < heroLetters.length; i++) {
 
     heroTitle.innerHTML += '<span>' + heroLetters[i] + '</span>'
   }
-
-  // lastTitle.style.opacity = "1";
-
-  // let last = heroText.match(/\s\w+$/i)[0];
-  // console.log(last);
 
   let item = 0;
   let timer = setInterval(onTick, 65);
@@ -34,23 +25,27 @@ let loadAnimation = setTimeout(function () {
   function onTick() {
 
     const span = heroTitle.querySelectorAll('span')[item];
-    console.log(span)
-
     span.classList.add('color');
     item++
     if (item === heroLetters.length) {
+      showArrow();
       complete();
       return;
     }
   };
 
+  function showArrow() {
+    iconHero.style.opacity = '1'
+  }
+
   function complete() {
     clearInterval(timer);
     timer = null;
   };
-}, 1000);
 
-goToTopBtn.style.display = "none";
+  goToTopBtn.style.display = "none";
+})
+
 
 //Hamburger menu
 
@@ -100,68 +95,50 @@ function scrollTop() {
 goToTopBtn.onclick = scrollTop;
 
 //Form actions
-window.onload = function () {
-  const contactForm = document.querySelector('form'),
-    formReq = contactForm.querySelectorAll("[required]"),
-    formStar = contactForm.getElementsByClassName('req'),
-    checkPermission = contactForm.querySelector('.permission'),
-    checkLabel = contactForm.querySelector('.check'),
-    formStatement = contactForm.querySelector('.statement');
 
-  if (checkPermission.checked == true) {
-    checkLabel.style.color = "rgb(50, 180, 140)";
+form_mod = this.document.querySelector('.form'),
+  form_el = form_mod.querySelector('form'),
+  form_req = form_mod.querySelectorAll('[required]');
+
+form_el.onsubmit = function (event) {
+  event.preventDefault();
+
+  form_el.classList.add('is-submitted');
+  var isError = false;
+
+  for (var i = 0; i < form_req.length; i++) {
+    if (form_req[i].checkValidity() != true) {
+      isError = true;
+    }
   }
 
-  //form submit
-  contactForm.onsubmit = function (event) {
-    event.preventDefault();
+  if (!isError) {
+    // send post
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        alert('form send');
 
-    contactForm.classList.add('is-submitted');
-    var error = false;
-
-    for (var i = 0; i < formReq.length; i++) {
-
-      if (formReq[i].checkValidity() != true) {
-        error = true;
-
-
-        for (star of formStar) {
-          star.style.color = "red";
-        }
-        if (checkPermission.checked == true) {
-          checkLabel.style.color = "rgb(50, 180, 140)";
-        }
+        form_el.classList.remove('is-submitted');
+        form_el.reset();
       }
-    }
-
-    if (!error) {
-      // base js send post
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-
-          formStatement.textContent = "Dziękujemy za zgłoszenie!"
-          formStatement.classList.remove('error');
-          formStatement.classList.add('correct');
-
-          contactForm.classList.remove('is-submitted');
-          setInterval(() => formStatement.style.opacity = 0, 3000);
-          for (star of formStar) {
-            star.style.color = "inherit";
-          }
-          contactForm.reset();
-        }
-      };
-      xhttp.open("POST", "https://httpstat.us/200", true);
-      xhttp.send(new FormData(contactForm));
-    } else {
-      // console.log('there are some not property validated fields')
-      formStatement.textContent = "Wypełnij pola poprawnie"
-      formStatement.classList.add('error');
-      formStatement.classList.remove('correct');
-      setInterval(() => formStatement.style.opacity = 0, 3000);
-    }
-
-
+    };
+    xhttp.open("POST", "https://httpstat.us/200", true);
+    xhttp.send(new FormData(form_el));
+  } else {
+    console.log('there are some not property validated fields')
   }
+
 }
+
+// FOOTER DATE
+
+let copyrights = document.getElementById('copyrights');
+
+function updateYear(el) {
+  const date = new Date();
+  const year = date.getFullYear();
+  el.textContent = year;
+};
+
+updateYear(copyrights);
